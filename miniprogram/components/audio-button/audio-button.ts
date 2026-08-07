c6469fca058504703e34991ce0e085a7be248794
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-07 20:11:59 +0800

    add miniprogram/components/audio-button/audio-button.ts

---
+++ miniprogram/components/audio-button/audio-button.ts
@@ -0,0 +1,17 @@
+Component({
+  properties: {
+    src: { type: String, value: '' },
+    icon: { type: String, value: '🔊' },
+    text: { type: String, value: '' },
+    type: { type: String, value: 'primary' },
+    disabled: { type: Boolean, value: false },
+  },
+  data: { playing: false },
+  methods: {
+    handleTap() {
+      if (this.properties.disabled) return;
+      // 触发父组件事件
+      this.triggerEvent('play', { src: this.properties.src });
+    },
+  },
+});
