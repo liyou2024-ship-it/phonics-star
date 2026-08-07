107b5aa78e1181515efdb8b58f52ed28d6e669aa
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-07 20:16:05 +0800

    add miniprogram/modules/speech/types.ts

---
+++ miniprogram/modules/speech/types.ts
@@ -0,0 +1,4 @@
+/**
+ * 语音模块内部类型（补充）
+ */
+export type { RecorderStatus, RecorderResult, IRecorder, ISpeechEvaluator, IAudioPlayer, PhonemeResult, SpeechEvaluationResult } from '../../types/speech';
