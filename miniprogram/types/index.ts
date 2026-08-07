33f509fec8862a322a7304a2cfbbb6a3891119a0
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-07 21:14:11 +0800

    add miniprogram/types/index.ts

---
+++ miniprogram/types/index.ts
@@ -0,0 +1,11 @@
+/**
+ * 类型定义统一导出
+ */
+
+export * from './phonics';
+export * from './course';
+export * from './exercise';
+export * from './speech';
+export * from './progress';
+export * from './reward';
+export * from './user';
