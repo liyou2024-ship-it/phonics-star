be242ab503373657217bf83f2906d120b408848f
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-08 11:33:35 +0800

    sync: miniprogram/utils/constants.js

---
+++ miniprogram/utils/constants.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.STEP_TYPE_NAMES = exports.LEVEL_NAMES = exports.APP_SLOGAN = exports.APP_NAME = void 0;
+/** 应用名称 */
+exports.APP_NAME = '自然拼读星';
+/** 应用口号 */
+exports.APP_SLOGAN = '每天10分钟，看词会读，听音会拼';
+/** 课程阶段名称映射 */
+exports.LEVEL_NAMES = {
+    lvl_alphabet_forest: '字母森林',
+    lvl_short_vowel_village: '短元音村',
+    lvl_consonant_castle: '辅音城堡',
+    lvl_long_vowel_ocean: '长元音海洋',
+    lvl_vowel_combo_valley: '组合音山谷',
+    lvl_reading_planet: '阅读星球',
+};
+/** 步骤类型中文名 */
+exports.STEP_TYPE_NAMES = {
+    phoneme_intro: '发音认识',
+    sound_discrimination: '听音选择',
+    phoneme_blending: '拼读合成',
+    word_segmenting: '单词拆音',
+    pronunciation: '跟读练习',
+    mini_game: '小游戏',
+    decodable_reading: '可解码阅读',
+    lesson_reward: '课程奖励',
+};
+//# sourceMappingURL=constants.js.map
