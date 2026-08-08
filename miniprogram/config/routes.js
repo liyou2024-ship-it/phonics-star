07cfe64b26426ede68dabb3719b4f4ab2e68ea9a
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-08 11:33:30 +0800

    sync: miniprogram/config/routes.js

---
+++ miniprogram/config/routes.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * 页面路由配置
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ROUTES = void 0;
+exports.ROUTES = {
+    HOME: '/pages/home/home',
+    COURSE_MAP: '/pages/course-map/course-map',
+    LESSON: '/pages/lesson/lesson',
+    PRACTICE: '/pages/practice/practice',
+    GAMES: '/pages/games/games',
+    GROWTH: '/pages/growth/growth',
+    ASSESSMENT: '/pages/assessment/assessment',
+    PARENT_REPORT: '/pages/parent-report/parent-report',
+    PROFILE: '/pages/profile/profile',
+    SETTINGS: '/pages/settings/settings',
+};
+//# sourceMappingURL=routes.js.map
