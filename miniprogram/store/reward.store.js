306d1488b2f6be1de87644cf6af31697e5dfd7fa
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-08 11:37:23 +0800

    sync: miniprogram/store/reward.store.js

---
+++ miniprogram/store/reward.store.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ * 奖励状态管理（对外入口）
+ * 重新导出 modules/rewards/reward.store
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.rewardStore = void 0;
+var reward_store_1 = require("../modules/rewards/reward.store");
+Object.defineProperty(exports, "rewardStore", { enumerable: true, get: function () { return reward_store_1.rewardStore; } });
+//# sourceMappingURL=reward.store.js.map
