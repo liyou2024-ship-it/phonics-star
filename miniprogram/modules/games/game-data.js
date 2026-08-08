00f93856654d58ac9a163d1f330dafae87e47e3e
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-08 11:35:44 +0800

    sync: miniprogram/modules/games/game-data.js

---
+++ miniprogram/modules/games/game-data.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.phonemeTrainStations = exports.wordFishingTargets = exports.whackMoleRounds = void 0;
+exports.whackMoleRounds = [
+    { target: 's', options: ['s', 'a', 't', 'p'] },
+    { target: 'a', options: ['a', 's', 'm', 't'] },
+    { target: 't', options: ['t', 'p', 'i', 'n'] },
+    { target: 'p', options: ['p', 'i', 'n', 's'] },
+    { target: 'i', options: ['i', 'n', 't', 'a'] },
+    { target: 'n', options: ['n', 's', 'a', 't'] },
+    { target: 'm', options: ['m', 'd', 'g', 'o'] },
+    { target: 'd', options: ['d', 'g', 'o', 'c'] },
+    { target: 'g', options: ['g', 'o', 'c', 'k'] },
+    { target: 'o', options: ['o', 'c', 'k', 'e'] },
+];
+exports.wordFishingTargets = [
+    { word: 'cat', fishes: ['cat', 'hat', 'bat', 'mat'] },
+    { word: 'dog', fishes: ['dog', 'log', 'fog', 'hog'] },
+    { word: 'sun', fishes: ['sun', 'fun', 'run', 'bun'] },
+    { word: 'pig', fishes: ['pig', 'big', 'dig', 'fig'] },
+    { word: 'bed', fishes: ['bed', 'red', 'fed', 'led'] },
+];
+exports.phonemeTrainStations = [
+    { word: 'sit', letters: ['s', 'i', 't'] },
+    { word: 'sat', letters: ['a', 's', 't'] },
+    { word: 'sun', letters: ['s', 'n', 'u'] },
+    { word: 'cat', letters: ['t', 'a', 'c'] },
+    { word: 'hat', letters: ['h', 't', 'a'] },
+];
+//# sourceMappingURL=game-data.js.map
