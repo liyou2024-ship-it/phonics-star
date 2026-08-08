7cc10e12210f01b8a56d4c865310797cfca9235c
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-08 11:33:21 +0800

    sync: miniprogram/types/phonics.js

---
+++ miniprogram/types/phonics.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * 自然拼读核心类型定义
+ * 包含音素、字素、单词的数据结构
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PhonemeCategory = void 0;
+/** 音素类别枚举 */
+exports.PhonemeCategory = {
+    VOWEL: 'vowel',
+    CONSONANT: 'consonant',
+    DIGRAPH: 'digraph',
+};
+//# sourceMappingURL=phonics.js.map
