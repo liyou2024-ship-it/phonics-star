91f45e8f23af44853f022a8d97f48128ff006378
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-08 11:35:55 +0800

    sync: miniprogram/data/decodable-readers.json.js

---
+++ miniprogram/data/decodable-readers.json.js
@@ -0,0 +1,74 @@
+module.exports = [
+  {
+    "id":"dr_cat_on_mat",
+    "title":"A Cat on a Mat",
+    "sentences":[
+      "A cat is on a mat.",
+      "The cat has a hat.",
+      "The cat is fat.",
+      "The cat sat and sat."
+    ],
+    "rules":["-at"],
+    "targetWordIds":["w_cat","w_mat","w_hat","w_sat","w_fat"],
+    "recommendedLevelId":"lvl_short_vowel_village",
+    "difficulty":1
+  },
+  {
+    "id":"dr_sam_bag",
+    "title":"Sam and the Bag",
+    "sentences":[
+      "Sam has a red bag.",
+      "The bag is on the bed.",
+      "Sam puts a map in the bag.",
+      "Sam puts a hat in the bag.",
+      "Sam gets in a van."
+    ],
+    "rules":["-an","-ap","-at"],
+    "targetWordIds":["w_map","w_hat","w_van","w_bed","w_red"],
+    "recommendedLevelId":"lvl_short_vowel_village",
+    "difficulty":1
+  },
+  {
+    "id":"dr_big_pig",
+    "title":"A Big Pig",
+    "sentences":[
+      "I see a big pig.",
+      "The pig can dig.",
+      "The pig has a wig.",
+      "The pig sits in the bin."
+    ],
+    "rules":["-ig","-in","-it"],
+    "targetWordIds":["w_big","w_pig","w_dig","w_wig","w_sit","w_bin"],
+    "recommendedLevelId":"lvl_short_vowel_village",
+    "difficulty":2
+  },
+  {
+    "id":"dr_dog_hop",
+    "title":"The Dog Can Hop",
+    "sentences":[
+      "I see a dog.",
+      "The dog can hop.",
+      "Hop to the top!",
+      "The dog is on a log."
+    ],
+    "rules":["-op","-og"],
+    "targetWordIds":["w_dog","w_hop","w_top","w_log"],
+    "recommendedLevelId":"lvl_short_vowel_village",
+    "difficulty":2
+  },
+  {
+    "id":"dr_sun_up",
+    "title":"The Sun Is Up",
+    "sentences":[
+      "The sun is up.",
+      "I run and run.",
+      "It is so fun!",
+      "But I go to the hut.",
+      "I cut a nut."
+    ],
+    "rules":["-un","-ut"],
+    "targetWordIds":["w_sun","w_run","w_fun","w_hut","w_cut","w_nut"],
+    "recommendedLevelId":"lvl_short_vowel_village",
+    "difficulty":2
+  }
+];
