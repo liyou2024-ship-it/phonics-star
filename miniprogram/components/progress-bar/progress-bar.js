7cce89bc50904346276f66b55408a442685c1d1b
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-08 11:34:04 +0800

    sync: miniprogram/components/progress-bar/progress-bar.js

---
+++ miniprogram/components/progress-bar/progress-bar.js
@@ -0,0 +1,10 @@
+"use strict";
+Component({
+    properties: {
+        percent: { type: Number, value: 0 },
+        color: { type: String, value: '#7c3aed' },
+        height: { type: Number, value: 12 },
+        showText: { type: Boolean, value: false },
+    },
+});
+//# sourceMappingURL=progress-bar.js.map
