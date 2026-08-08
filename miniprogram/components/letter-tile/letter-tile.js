caa26a1693eb31ec924107aeb08a6bf7e5d235af
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-08 11:33:52 +0800

    sync: miniprogram/components/letter-tile/letter-tile.js

---
+++ miniprogram/components/letter-tile/letter-tile.js
@@ -0,0 +1,36 @@
+"use strict";
+const VOWELS = new Set(['a', 'e', 'i', 'o', 'u']);
+Component({
+    properties: {
+        letter: { type: String, value: '' },
+        size: { type: String, value: 'normal' },
+        selected: { type: Boolean, value: false },
+        disabled: { type: Boolean, value: false },
+    },
+    data: {
+        isVowel: false,
+        tileClass: '',
+    },
+    observers: {
+        'letter, selected, disabled, size'(l, s, d, sz) {
+            const cls = [`tile--${sz}`];
+            if (s)
+                cls.push('tile--selected');
+            if (d)
+                cls.push('tile--disabled');
+            if (VOWELS.has(l.toLowerCase()))
+                cls.push('tile--vowel');
+            else if (l)
+                cls.push('tile--consonant');
+            this.setData({ isVowel: VOWELS.has(l.toLowerCase()), tileClass: cls.join(' ') });
+        },
+    },
+    methods: {
+        onTap() {
+            if (!this.data.disabled) {
+                this.triggerEvent('tap', { letter: this.data.letter });
+            }
+        },
+    },
+});
+//# sourceMappingURL=letter-tile.js.map
