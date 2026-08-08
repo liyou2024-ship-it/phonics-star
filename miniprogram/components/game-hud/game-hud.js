99003d7337a39a760ac641274373fc7f574bdea8
liyou2024-ship-it <liyou2024@outlook.com>
2026-08-08 11:33:56 +0800

    sync: miniprogram/components/game-hud/game-hud.js

---
+++ miniprogram/components/game-hud/game-hud.js
@@ -0,0 +1,19 @@
+"use strict";
+Component({
+    properties: {
+        round: { type: Number, value: 1 },
+        maxRounds: { type: Number, value: 10 },
+        score: { type: Number, value: 0 },
+        combo: { type: Number, value: 0 },
+        timeLeft: { type: Number, value: 0 },
+    },
+    data: {
+        comboVisible: false,
+    },
+    observers: {
+        'combo'(val) {
+            this.setData({ comboVisible: val > 1 });
+        },
+    },
+});
+//# sourceMappingURL=game-hud.js.map
